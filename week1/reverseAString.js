b60202d9232596371d0ae47570e2bae8ee0da867
Suraj Jha <123754719+Suraj-jha7@users.noreply.github.com>
2025-09-19 03:56:02 +0000

    recursion

--- week1/reverseAString.js
+++ week1/reverseAString.js
@@ -18,8 +18,22 @@ class DSA {
         return false;
     }
 
+    // 1*2*3*4*5
     factorial(num) {
-        
+        if(num<1)return 0;
+        let ans = 1;
+        for(let i = 1; i<=num; i++) {
+            ans = ans*i;
+        }
+        return ans;
+    }
+
+    factorialRec(num) {
+        if(num == 1 || num == 0 )return num;
+
+        const ans1 = this.factorialRec(num-1);
+        return ans1*num
+
     }
 }
 
@@ -27,3 +41,4 @@ let t1 = new DSA();
 // t1.reverseString("car");
 //  t1.reverseString2("carjack");
 // console.log(t1.pallindrome("madamm"));
+console.log(t1.factorialRec(5));
