f98ea42e80e7f14473750773bf9d2c6c038d85bc
Suraj Jha <123754719+Suraj-jha7@users.noreply.github.com>
2025-09-22 04:34:08 +0000

    anagram

--- week1/getLargest.js
+++ week1/getLargest.js
@@ -95,6 +95,26 @@ class DSA {
         console.log(charCount);
     }
 
+    anagramCheck(str1, str2) {
+        let feqCount = new Map();
+        for(let i = 0; i<str1.length; i++) {
+            feqCount.get(str1.charAt(i))?feqCount.set(str1.charAt(i), feqCount.get(str1.charAt(i))+1): feqCount.set(str1.charAt(i), 1);
+        }
+        console.log("102 -",feqCount);
+        for(let i = 0; i<str2.length;i++) {
+            if(feqCount.get(str2.charAt(i)))
+             feqCount.set(str2.charAt(i), feqCount.get(str2.charAt(i))-1);
+            else {
+                return false;
+            }
+        }
+        for(let [key, value] of feqCount.entries()) {
+            if(value>0)return false;
+        }
+        console.log(feqCount);
+        return true;
+    }
+
 }
 
 let t1 = new DSA();
@@ -105,4 +125,5 @@ let t1 = new DSA();
 // console.log(t1.findDuplicates([1, 3, 4, 2, 2]));
 // console.log(t1.findDuplicatesWithoutSorting([1, 3, 4, 2, 2, 19, 10, 19, 19]));
 // console.log(t1.findDuplicatesWithoutSortingwithMao([1, 3, 4, 2, 2, 19, 10, 19, 19]));
-t1.frequencyCharaterr("fkajfafaf");
+// t1.frequencyCharaterr("fkajfafaf");
+console.log(t1.anagramCheck("listen", "listenn"))
